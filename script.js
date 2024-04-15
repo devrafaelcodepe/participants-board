6daf2d31042fda7de37488e78f655c4146583b14
devrafaelcodepe <122902210+devrafaelcodepe@users.noreply.github.com>
2024-04-15 15:13:17 -0300

    test scroll arrow

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('scrollUp').addEventListener('click', function() {
-    console.log('up')
     window.scrollBy(0, -50);
 });
 
 document.getElementById('scrollDown').addEventListener('click', function() {
-    console.log('down')
     window.scrollBy(0, 50); 
 });
 
